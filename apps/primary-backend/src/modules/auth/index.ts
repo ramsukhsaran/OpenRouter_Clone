f47bdffc392c1fa9346ac284ab146e5b70aa4414
ramsukhsaran <ramsukhsaran0834@gmail.com>
2026-03-08 01:47:03 +0530

    Implement authentication module with JWT support, add user and API key models, and set up database migrations

--- apps/primary-backend/src/modules/auth/index.ts
+++ apps/primary-backend/src/modules/auth/index.ts
@@ -1,16 +1,30 @@
-import { Elysia } from 'elysia'
-import { AuthModel } from './models'
-import { AuthService } from './service'
+import { Elysia } from "elysia";
+import { AuthModel } from "./models";
+import { AuthService } from "./service";
 
+import jwt from "@elysiajs/jwt";
 
-export const auth = new Elysia({ prefix: '/auth' })
+export const auth = new Elysia({ prefix: "/auth" })
+  .use(
+    jwt({
+      name:'jwt',
+      secret:process.env.JWT_SECRET!,
+    })
+  )
   .post(
-    '/signup',
-    async ({ body }) => {
-      const userid = await AuthService.signUp(body.email, body.password);
-      return {
-        id: userid,
-      };
+    "/signup",
+    async ({ body, status }) => {
+      try {
+        const userid = await AuthService.signUp(body.email, body.password);
+        return {
+          id: userid,
+        };
+      } catch (error) {
+        console.log("Error in signup", error);
+        return status(400, {
+          message: "Error in signup",
+        });
+      }
     },
     {
       body: AuthModel.signUpSchema,
@@ -20,15 +34,32 @@ export const auth = new Elysia({ prefix: '/auth' })
       },
     },
   )
-  .post('/signin', async ({ body }) => {
-    const token = await AuthService.signIn(body.email, body.password);
-    return {
-      token: token,
-    };
-  }, {
-    body: AuthModel.signInSchema,
-    response: {
-      200: AuthModel.signInResponseSchema,
-      400: AuthModel.signInfailedResponseSchema,
+  .post(
+    "/signin",
+    async ({ jwt, body, cookie: { auth }, status }) => {
+      const { correctDetails, userId } = await AuthService.signIn(body.email, body.password);
+      if (correctDetails && userId) {
+        const value = await jwt.sign({ userId })
+        auth.set({
+          value,
+          httpOnly: true,
+          maxAge: 60 * 60 * 24, // 1 day
+        })
+
+        return {
+          message: "SignIn successful"
+        }
+      } else {
+        return status(400, {
+          message: "Invalid email or password"
+        })
+      }
     },
-  });
+    {
+      body: AuthModel.signInSchema,
+      response: {
+        200: AuthModel.signInResponseSchema,
+        400: AuthModel.signInFailedResponseSchema
+      },
+    },
+  );
--- apps/primary-backend/src/modules/auth/service.ts
+++ apps/primary-backend/src/modules/auth/service.ts
@@ -1,10 +1,27 @@
-
+import { prisma } from "db";
+import {jwt} from "@elysiajs/jwt";
 
 export abstract class AuthService {
-    static async  signUp(email: string, password: string):Promise<string>{
-          return "1234567";
-    }
-    static async signIn(email: string, password: string):Promise<string>{
-        return "toekn-746gt56"
-    }
+  static async signUp(email: string, password: string): Promise<string> {
+    const user = await prisma.user.create({
+      data: {
+        email: email,
+        password: await Bun.password.hash(password),
+      },
+    });
+
+    return user.id.toString();
+  }
+  static async signIn(email: string, password: string): Promise<{correctDetails:boolean,userId?:string}> {
+     const user = await prisma.user.findFirst({
+        where:{email:email}
+     })
+     if(!user){
+        return {correctDetails:false}
+     }
+     if(!await Bun.password.verify(password,user.password)){
+        return {correctDetails:false}
+     }
+     return {correctDetails:true,userId:user.id.toString()}
+  }
 }
